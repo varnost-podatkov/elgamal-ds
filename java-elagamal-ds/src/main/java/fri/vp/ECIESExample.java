2b8bb2030fb80fd8f2e47815b248d76a202e1263
David Jelenc <david.jelenc@fri.uni-lj.si>
2023-12-19 00:52:56 +0100

    Pripravi naloge

--- java-elagamal-ds/src/main/java/fri/vp/ECIESExample.java
+++ java-elagamal-ds/src/main/java/fri/vp/ECIESExample.java
@@ -1,64 +1,27 @@
 package fri.vp;
 
-import fri.isp.Agent;
-
-import javax.crypto.Cipher;
-import javax.crypto.KeyAgreement;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.*;
-import java.security.spec.X509EncodedKeySpec;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 public class ECIESExample {
     public record Ciphertext(byte[] pk, byte[] iv, byte[] ct) {
     }
 
     public static KeyPair gen() throws Exception {
-        return KeyPairGenerator.getInstance("X25519").generateKeyPair();
+        return null;
     }
 
     public static Ciphertext encrypt(PublicKey pk, byte[] plaintext) throws Exception {
-        final KeyPair ephemeralKey = gen();
-
-        final KeyAgreement ka = KeyAgreement.getInstance("XDH");
-        ka.init(ephemeralKey.getPrivate());
-        ka.doPhase(pk, true);
-
-        final MessageDigest sha = MessageDigest.getInstance("SHA-256");
-        sha.update(ephemeralKey.getPublic().getEncoded());
-        sha.update(ka.generateSecret());
-        final byte[] sharedBytes = sha.digest();
-
-        final SecretKeySpec aesKey = new SecretKeySpec(sharedBytes, "ChaCha20");
-
-        final Cipher gcm = Cipher.getInstance("ChaCha20-Poly1305");
-        gcm.init(Cipher.ENCRYPT_MODE, aesKey);
-        final byte[] ct = gcm.doFinal(plaintext);
-
-        return new Ciphertext(ephemeralKey.getPublic().getEncoded(), gcm.getIV(), ct);
+        return new Ciphertext(null, null, null);
     }
 
     public static byte[] decrypt(PrivateKey sk, byte[] pk, byte[] iv, byte[] ct) throws Exception {
-        final PublicKey epk = KeyFactory.getInstance("XDH").generatePublic(new X509EncodedKeySpec(pk));
-
-        final KeyAgreement ka = KeyAgreement.getInstance("XDH");
-        ka.init(sk);
-        ka.doPhase(epk, true);
-
-        final MessageDigest sha = MessageDigest.getInstance("SHA-256");
-        sha.update(pk);
-        sha.update(ka.generateSecret());
-        final byte[] sharedBytes = sha.digest();
-
-        final SecretKeySpec aesKey = new SecretKeySpec(sharedBytes, "ChaCha20");
-
-        final Cipher gcm = Cipher.getInstance("ChaCha20-Poly1305");
-        gcm.init(Cipher.DECRYPT_MODE, aesKey, new IvParameterSpec(iv));
-        return gcm.doFinal(ct);
+        return null;
     }
 
     public static void main(String[] args) throws Exception {
--- java-elagamal-ds/src/main/java/fri/vp/RSAPSSSignature.java
+++ java-elagamal-ds/src/main/java/fri/vp/RSAPSSSignature.java
@@ -5,30 +5,22 @@ import fri.isp.Agent;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.*;
-import java.security.spec.MGF1ParameterSpec;
-import java.security.spec.PSSParameterSpec;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 public class RSAPSSSignature {
 
     public static KeyPair gen() throws Exception {
-        return KeyPairGenerator.getInstance("RSASSA-PSS").generateKeyPair();
+        return null;
     }
 
     public static byte[] sign(PrivateKey key, byte[] message) throws Exception {
-        final Signature signer = Signature.getInstance("RSASSA-PSS");
-        signer.setParameter(new PSSParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256, 32, 1));
-        signer.initSign(key);
-        signer.update(message);
-        return signer.sign();
+        return null;
     }
 
     public static boolean verify(PublicKey key, byte[] message, byte[] signature) throws Exception {
-        final Signature verifier = Signature.getInstance("RSASSA-PSS");
-        verifier.setParameter(new PSSParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256, 32, 1));
-        verifier.initVerify(key);
-        verifier.update(message);
-        return verifier.verify(signature);
+        return false;
     }
 
     public static void main(String[] args) throws Exception {
